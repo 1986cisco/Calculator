225099d6d4fe532b0448cbcf7a8b146eec8e446c
1986cisco <cisco-sv@gmx.de>
2016-04-10 14:17:12 +0200

    its done2

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,7 @@ public class Calculator{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		Sytem.out.println("Bitte eingeben [+-*/] zahl1 zahl2: ");
+		String eingabe = scanner.next();
 		String operator = scanner.next();
 		int zahl1 = scanner.nextInt();
 		int zahl2 = scanner.nextInt();
